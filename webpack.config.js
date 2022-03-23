47bebf5d91c72864906fa3da0fafabd9f3da9c1a
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2022-03-23 15:49:21 -0400

    fix: Fix webpack config to support latest storybook version

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         path: path.join(__dirname, "dist"),
         filename: "index.js",
         library: {
-            type: 'umd'
+            type: 'commonjs',
         }
     },
     module: {
